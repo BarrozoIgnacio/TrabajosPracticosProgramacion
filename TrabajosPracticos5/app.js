a74ac9a7ff11035c41d76df8a0148c144977226c
alumno <alumno@Instituto.local>
2026-06-04 10:39:40 -0300

    trabajo variables

--- TrabajosPracticos5/app.js
+++ TrabajosPracticos5/app.js
@@ -1,5 +1,10 @@
-//declarar variable
-let numero1
-//asiganarvalor
-numero1
 
+console.log(numero1 + numero1 + numero2)
+let suma
+let division
+let multiplicacion
+let resta
+let numero1 = 12
+let numero2 = 20
+
+suma = numero1 + numero2
