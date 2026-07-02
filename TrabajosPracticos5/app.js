218dae67aa740869da644ba472af5d459d86dd2c
sofita b <52060292@ivt.edu.ar>
2026-07-02 10:53:04 -0300

    TrabajoPractico6

--- TrabajosPracticos5/app.js
+++ TrabajosPracticos5/app.js
@@ -1,10 +1,42 @@
+let numero1 = 20
+let numero2 = 5
+let suma = numero1 + numero2
+confirm("El resultado de la suma es: " + suma)
+let resta = numero1 - numero2
+confirm("El resultado de la resta es: " + resta)
+let multiplicacion = numero1 * numero2
+console.log("El resultado de la multiplicacion es: " + multiplicacion)
+let division = numero1 / numero2
+console.log("El resultado de la division es: " + division)
 
-console.log(numero1 + numero1 + numero2)
-let suma
-let division
-let multiplicacion
-let resta
-let numero1 = 12
-let numero2 = 20
 
-suma = numero1 + numero2
+
+let nombreAlumno = "Juan"
+let apellidoAlumno = "Sanchez"
+let edadAlumno = 14
+console.log("El alumno : " + nombreAlumno + " " + apellidoAlumno + " tiene " + edadAlumno + " años")
+
+
+
+
+let precio1 = 100
+let precio2 = 200
+let precio3 = 300
+let total = precio1 + precio2 + precio3
+console.log("El total a pagar es : " + total)
+
+
+
+
+let comidaFavorita = "Milanesa"
+let colorFavorito = "Negro"
+let numeroFavorito = 67
+console.log("Mi comida favorita es " + comidaFavorita + ", me gusta el color " + colorFavorito + " y mi número favorito es " + numeroFavorito)
+
+
+
+
+let pais = "Argentina"
+console.log(pais)
+pais = "Brasil"
+console.log(pais)
