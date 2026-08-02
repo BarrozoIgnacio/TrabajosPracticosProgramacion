50b139502d955b356955857b5adee29ed0556465
Barrozo <52211164@ivt.edu.ar>
2026-08-02 15:45:01 -0300

    tp6

--- TrabajoPractico6/app.js
+++ TrabajoPractico6/app.js
@@ -1,12 +1,41 @@
-let botonFondo = document.querySelector("")
-let botonTexto = document.querySelector("")
-let parrafo = document.querySelector("")
-let botoncolor = document.querySelector("")
-let botonLetra = document.querySelector("")
-let botonTamaño = document.querySelector("")
-let botonReiniciar = document.querySelector("")
 
-botonFondo.onclick = function () {
-    
-}
-    
+let parrafo = document.querySelector("#parrafo");
+let btnFondo = document.querySelector("#btnFondo");
+let btnTexto = document.querySelector("#btnTexto");
+let btnColor = document.querySelector("#btnColor");
+let btnLetra = document.querySelector("#btnLetra");
+let btnTamano = document.querySelector("#btnTamano");
+let btnReiniciar = document.querySelector("#btnReiniciar");
+let textoOriginal = "Este parrafo puede ser modificado.";
+let colorTextoOriginal = "black";
+let colorFondoOriginal = "white";
+let fuenteOriginal = "Arial";
+let tamanoOriginal = "20px";
+
+btnFondo.onclick = function () {
+    parrafo.style.backgroundColor = "blue";
+};
+
+btnTexto.onclick = function () {
+    parrafo.textContent = "Cambiaste el texto";
+};
+
+btnColor.onclick = function () {
+    parrafo.style.color = "red";
+};
+
+btnLetra.onclick = function () {
+    parrafo.style.fontFamily = "Impact";
+};
+
+btnTamaño.onclick = function () {
+    parrafo.style.fontSize = "30px";
+};
+
+btnReiniciar.onclick = function () {
+    parrafo.textContent = textoOriginal;
+    parrafo.style.color = colorTextoOriginal;
+    parrafo.style.backgroundColor = colorFondoOriginal;
+    parrafo.style.fontFamily = fuenteOriginal;
+    parrafo.style.fontSize = tamañoOriginal;
+};
