ff14c7bec50c0e5a6506fad4289e99f6bff9adbf
Barrozo <52211164@ivt.edu.ar>
2026-08-06 18:40:50 -0300

    tp7

--- TrabajoPractico7/app.js
+++ TrabajoPractico7/app.js
@@ -1,1 +1,30 @@
-let edadCliente = 18
+//Ejercicio 1
+let edadCliente = 18;
+
+let boton1 = document.querySelector("#boton1");
+
+boton1.onclick = function() {
+
+    if (edadCliente >= 18) {
+        document.querySelector("#parrafo1").textContent = "Eres mayor de edad.";
+    } else {
+        document.querySelector("#parrafo1").textContent = "Eres menor de edad.";
+    }
+
+};
+
+//Ejercicio 2
+let nombreUsuario = "Nahuel";
+
+let boton2 = document.querySelector("#boton2");
+let parrafo2 = document.querySelector("#parrafo2");
+
+boton2.onclick = verificarNombre;
+
+function verificarNombre() {
+    if (nombreUsuario === "Nahuel") {
+        parrafo2.textContent = "Bienvenido Nahuel ¿como estas?";
+    } else {
+        parrafo2.textContent = "Bienvenido usuario";
+    }
+}
